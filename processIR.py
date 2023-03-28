009673cdbb63b0cc4b04083e78c5c7ec4eb3dc50
Ben Wesch <ben.wes@Bens-Air.fritz.box>
2023-03-28 16:46:41 +0200

    allow 24bit input and arbitrary channel count

--- processIR.py
+++ processIR.py
@@ -6,10 +6,11 @@
 
 from scipy import signal
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import wave
+import wavfile
+import ffmpeg
 from pydub import AudioSegment
-import wavfile                  #a custom script to be copied to the same folder
 
 # AUX functions 
 def ratio(dB):
@@ -29,103 +30,79 @@ def filter20_20k(x, sr): # filters everything outside out 20 - 20000 Hz
     sos = signal.butter(5, [20.0 / nyq, 20000.0 / nyq], btype='band', output='sos')
     return signal.sosfilt(sos, x)
 
-#extracts one channel from a multichannel file and saves it to a mono file
-#usage: 
-# wav = wave.open(WAV_FILENAME) 
-# separate_wav_channel('ch1.wav', wav, 0) #(first channel)
-# separate_wav_channel('ch1.wav', wav, 2) #(third channel)
+def normalize(data, bits):
+    normfactor = 1.0
+    if bits == 8 or bits == 16 or bits == 24:
+        normfactor = 2 ** (bits-1)
+    data = np.float32(data) * 1.0 / normfactor
+    return data
 
-def extract_wav_channel(fn, wav, channel):
-    '''
-    Take Wave_read object as an input and save one of its
-    channels into a separate .wav file.
-    '''
-    # Read data
-    nch   = wav.getnchannels()
-    depth = wav.getsampwidth()
-    wav.setpos(0)
-    sdata = wav.readframes(wav.getnframes())
+# 24bit -> 32bit conversion from https://github.com/mgeier/python-audio/blob/master/audio-files/audio-files-with-wave.ipynb
+def pcm24to32_bytearray(data, channels=1): 
+    if len(data) % 3 != 0:
+        raise ValueError('Size of data must be a multiple of 3 bytes')
 
-    # Extract channel data (24-bit data not supported)
+    size = len(data) // 3
+    
+    # reserve memory (initialized with null bytes):
+    temp = bytearray(size * 4)
+
+    for i in range(size):
+        newidx = i * 4 + 1
+        oldidx = i * 3
+        temp[newidx:newidx+3] = data[oldidx:oldidx+3]
+
+    return np.frombuffer(temp, dtype='<i4').reshape(-1, channels)
+
+def extract_wav_channel(input_wav, channel):
+    '''
+    Take Wave_read object as an input and get one of its channels
+    '''
+    input_wav.setpos(0)
+
+    nch   = input_wav.getnchannels()
+    depth = input_wav.getsampwidth()
+    sdata = input_wav.readframes(input_wav.getnframes())
+    if depth == 3:
+        sdata = pcm24to32_bytearray(sdata, channels=nch)
+        depth = 4
+
+    # Extract channel data
     typ = { 1: np.uint8, 2: np.uint16, 4: np.uint32 }.get(depth)
     if not typ:
-        raise ValueError("sample width {} not supported".format(depth))
-    if channel >= nch:
-        raise ValueError("cannot extract channel {} out of {}".format(channel+1, nch))
-    #print ("Extracting channel {} out of {} channels, {}-bit depth".format(channel+1, nch, depth*8))
+        raise ValueError(f'sample width {depth} not supported'.format(depth))
     data = np.fromstring(sdata, dtype=typ)
-    ch_data = data[channel::nch]
+    ch_data = normalize(data[channel::nch], depth*8)
 
-    # Save channel to a separate file
-    outwav = wave.open(fn, 'w')
-    outwav.setparams(wav.getparams())
-    outwav.setnchannels(1)
-    outwav.writeframes(ch_data.tostring())
-    outwav.close()
+    # return channel data
+    return ch_data
     
-def process(SWEEPFILE, wav_file, OUTFILE, channels_nr):
-    
-    #SWEEP VISUALISATION
+def process(SWEEPFILE, WAVFILE, OUTFILE):
     #sweep_visualisation(SWEEPFILE)
     
-    #first 4ch channel extraction to mono files
+    wav   = wave.open(WAVFILE)
+    nch   = wav.getnchannels()
+    sr    = wav.getframerate()
 
-    WAV_FILENAME = wav_file
-    wav = wave.open(WAV_FILENAME)
-    
-    if channels_nr == 1:
-        h, sr = calculate_IR(SWEEPFILE, wav_file, OUTFILE)
-        visualise_IR(h,sr)
-        print("Exported mono IR file {} " .format(OUTFILE))
-    if channels_nr == 2:
-        extract_wav_channel('ch1.wav', wav, 0)
-        h1, sr1 = calculate_IR(SWEEPFILE, 'ch1.wav', 'ir1.wav')
-        visualise_IR(h1,sr1)
-        extract_wav_channel('ch2.wav', wav, 1)
-        h2, sr2 = calculate_IR(SWEEPFILE, 'ch2.wav', 'ir2.wav')
-        visualise_IR(h2,sr2)
-        
-        # load individual channels and create a stereo IR file
-        left_channel = AudioSegment.from_wav("ir1.wav")
-        right_channel = AudioSegment.from_wav("ir2.wav")
-
-        stereo_sound = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
-        # simple export
-        file_handle = stereo_sound.export(OUTFILE, format="wav")
-        
-        print("Exported IR stereo file {} " .format(OUTFILE))
-        #print ("Extracting channel {} out of {} channels, {}-bit depth".format(channel+1, nch, depth*8))
-        
-    if channels_nr == 4:
-        extract_wav_channel('ch1.wav', wav, 0)
-        h1, sr1 = calculate_IR(SWEEPFILE, 'ch1.wav', 'ir1.wav')
-        visualise_IR(h1,sr1)
-        extract_wav_channel('ch2.wav', wav, 1)
-        h2, sr2 = calculate_IR(SWEEPFILE, 'ch2.wav', 'ir2.wav')
-        visualise_IR(h2,sr2)
-        extract_wav_channel('ch3.wav', wav, 2)
-        h3, sr3 = calculate_IR(SWEEPFILE, 'ch3.wav', 'ir3.wav')
-        visualise_IR(h3,sr3)
-        extract_wav_channel('ch4.wav', wav, 3)
-        h4, sr4 = calculate_IR(SWEEPFILE, 'ch4.wav', 'ir4.wav')
-        visualise_IR(h4,sr4)
-
-        # load individual channels and create a multichannel IR file
-        first_channel = AudioSegment.from_wav("ir1.wav")
-        second_channel = AudioSegment.from_wav("ir2.wav")
-        third_channel = AudioSegment.from_wav("ir3.wav")
-        fourth_channel = AudioSegment.from_wav("ir4.wav")
-
-        four_ch_sound = AudioSegment.from_mono_audiosegments(first_channel, second_channel, third_channel, fourth_channel)
-        # simple export
-        file_handle = four_ch_sound.export(OUTFILE, format="wav")
-        print("Exported 4channels IR file {} " .format(OUTFILE))
-
-def calculate_IR(SWEEPFILE, RECFILE, OUTFILE):  #per mono file
-
+    print(f'{nch} channels found')
+    channels = []
     #load previous files and get sample rate, bitrate and data arrays "a" and "b"
-    sr, a, br = wavfile.read(SWEEPFILE, normalized=True)
-    sr, b, br = wavfile.read(RECFILE, normalized=True)
+    _ , sweep, _ = wavfile.read(SWEEPFILE, normalized=True)
+
+    for channel in range(nch):
+        recording = extract_wav_channel(wav, channel)
+        filename = str(channel)+OUTFILE
+        h, sr = calculate_IR(sweep, recording, sr, filename)
+        visualise_IR(h, sr)
+        channels.append(AudioSegment.from_wav(filename))
+        
+    AudioSegment.from_mono_audiosegments(*channels).export(OUTFILE, format="wav")
+    print(f'Exported {len(channels)}-channel IR file {OUTFILE}')
+
+def calculate_IR(SWEEP, RECORDING, sr, OUTFILE):  #per mono file
+
+    a = SWEEP
+    b = RECORDING
     
     # Deconvolution 
     # a is the input sweep signal, h the impulse response, and b the microphone-recorded signal. 
@@ -137,25 +114,22 @@ def calculate_IR(SWEEPFILE, RECFILE, OUTFILE):  #per mono file
     b = padarray(b, sr*50, before=sr*10)
     h = np.zeros_like(b)
 
-    h = np.zeros_like(b)
-
     b1 = filter20_20k(b, sr)
 
     ffta = np.fft.rfft(a)
-
     fftb = np.fft.rfft(b1)
     ffth = fftb / ffta
+    
     h1 = np.fft.irfft(ffth)
-
     h1 = filter20_20k(h1, sr)
 
     h = h1
-
     h = h[:10 * sr]
     h *= ratio(dB=40)
 
-    #write to file
-    wavfile.write(OUTFILE, sr, h, normalized=True, bitrate=16)
+    # write to file
+    # wavfile.write(OUTFILE, sr, h, normalized=True, bitrate=16)
+    # print(f'Exported mono IR file {OUTFILE}')
     
     return h, sr
 
@@ -188,7 +162,6 @@ def sweep_visualisation(SWEEPFILE):
     sr, a, br = wavfile.read(SWEEPFILE, normalized=True)
     
     #VISUALIZE ORIGINAL SWEEP (mono)
-
 
     #we need to extract sample rate and number of samples to work with the file
     n_samples = a.size
